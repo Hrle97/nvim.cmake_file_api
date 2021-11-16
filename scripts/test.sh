3813e7c08dc5aef216b52930c329f534c014cd60
Hrvoje Juric <32d9c8c6-8118-45e7-857e-8b45522f395d@anonaddy.me>
2021-11-16 12:27:21 +0100

    test script refactor

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,20 +6,32 @@ root_path="$(realpath -e "${BASH_SOURCE[0]}" | xargs dirname | xargs dirname)"
 test_root_path="$root_path/test"
 example_root_path="$root_path/example"
 
+declare -g cmake_root_path test_init_path
+cmake_root_path="$test_root_path/cmake"
+test_init_path="$test_root_path/lua/init.lua"
+
 function test()
 {
-  echo -e "\nTESTING BUILD: '$1'\n"
+  echo -e "\nBUILD: '$1'"
+
+  echo -e "\nLS DIR:\n"
+  dirname $1 | xargs ls -la
+
+  echo -e "\nID:\n"
+  id
+
+  echo -e "\nRUNNING TESTS\n"
   if nvim --headless --clean \
       -c "lua vim.g.root_path = '$root_path'" \
       -c "lua vim.g.test_root_path = '$test_root_path'" \
       -c "lua vim.g.example_root_path = '$example_root_path'" \
-      -c "lua vim.g.cmake_source_path = '$test_root_path/cmake/'" \
+      -c "lua vim.g.cmake_source_path = '$cmake_root_path'" \
       -c "lua vim.g.cmake_build_path = '$1'" \
       -c "lua vim.o.runtimepath = 
             vim.o.runtimepath .. ',' .. 
             vim.g.root_path .. ',' .. 
             vim.g.test_root_path" \
-      -c "luafile $root_path/test/lua/init.lua" 2>&1; then
+      -c "luafile $test_init_path" 2>&1; then
     echo -e "\nBUILD '$1' PASSED!"
   else
     echo -e "\nBUILD '$1' FAILED!"
