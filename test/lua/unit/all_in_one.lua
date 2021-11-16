e9048c0ebc841d4cf0212cc6de73d1b58fc57147
Hrvoje Juric <32d9c8c6-8118-45e7-857e-8b45522f395d@anonaddy.me>
2021-11-16 15:21:46 +0100

    print write error that fails

--- test/lua/unit/all_in_one.lua
+++ test/lua/unit/all_in_one.lua
@@ -1,6 +1,9 @@
 local cmake_file_api = require "nvim.cmake_file_api"
 
-local did_write_query = cmake_file_api.write_all_queries(build)
+local did_write_query, write_err = cmake_file_api.write_all_queries(build)
+if not did_write_query then
+  print(write_err)
+end
 expect(did_write_query)
 
 cmake.configure()
